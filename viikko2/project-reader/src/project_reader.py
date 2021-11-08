70eeb9747a985d83f113f87027847fc6a88c8006
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-11-08 15:35:48 +0200

    Push ex. 2

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,24 @@
+import toml
+from urllib import request
+from project import Project
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        # print("Content:")
+        # print(content)
+
+        parsed_toml = toml.loads(content)
+        # print("Parsed:")
+        # print(parsed_toml)
+
+        dependencies = list(parsed_toml["tool"]["poetry"]["dependencies"])
+        dev_dependencies = list(parsed_toml["tool"]["poetry"]["dev-dependencies"])
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project("Test name", "Test description", dependencies, dev_dependencies)
