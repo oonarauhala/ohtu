f9151c2f2f203140a9d6438d79d9472eaa2ed2c9
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-11-26 19:39:04 +0200

    5.5: Add test for cart total with two items

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -21,9 +21,18 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipa)
         self.assertEqual(self.kori.hinta(), leipa.hinta())
 
-    def test_kahde_tuotteen_lisayksen_jalkeen_korissa_2_tuotetta(self):
+    def test_kahden_tuotteen_lisayksen_jalkeen_korissa_2_tuotetta(self):
         leipa = Tuote("Leipa", 2)
         porkkana = Tuote("Porkkana", 1)
         self.kori.lisaa_tuote(leipa)
         self.kori.lisaa_tuote(porkkana)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisayksenjalkeen_korin_hinta_sama_kuin_kahden_tuotteen_summa(
+        self,
+    ):
+        leipa = Tuote("Leipa", 2)
+        porkkana = Tuote("Porkkana", 1)
+        self.kori.lisaa_tuote(leipa)
+        self.kori.lisaa_tuote(porkkana)
+        self.assertEqual(self.kori.hinta(), 3)
