0dba197bf3ff6464483bc19b16d0e2262a3a83c7
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-09 20:28:18 +0200

    Push ex.3

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
 
 
 def main():
@@ -8,7 +8,7 @@ def main():
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(HasFewerThan(1, "goals"), PlaysIn("NYR"))
+    matcher = Or(HasAtLeast(30, "goals"), HasAtLeast(50, "assists"))
 
     for player in stats.matches(matcher):
         print(player)
