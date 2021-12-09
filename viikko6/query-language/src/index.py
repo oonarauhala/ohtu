4a0766f86894efdbe4c3761ab0fd29d531317a15
Oona Rauhala <oona.e.rauhala@gmail.com>
2021-12-09 20:04:51 +0200

    Push ex.2

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,18 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(HasFewerThan(1, "goals"), PlaysIn("NYR"))
+
+    for player in stats.matches(matcher):
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
